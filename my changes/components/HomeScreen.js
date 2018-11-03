baa98f685bc0538ae8938d1860342343da71225d
Cristina <cristyevr94@gmail.com>
2018-11-03 18:54:36 -0400

    connect pages

---
+++ my changes/components/HomeScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, View, Text, Image } from 'react-native';
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'NBSee',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'top' }}>
+        <Text>Profile</Text>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{uri: 'https://i.ytimg.com/vi/AZ2ZPmEfjvU/maxresdefault.jpg'}}
+        />
+        <Text >Profile</Text>
+        <Button
+          title="Information:"
+          color="#841584"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Law & Order"
+          color="#841584"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="The Good PLace"
+          color="#841584"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
