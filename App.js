a1be2c3716899870d9de2d21baf02647692b45fa
alemasserrano <mmass004@fiu.edu>
2018-11-03 17:41:42 -0400

    created routes for views

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+import HelloWorld from './components/HelloWorld';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const App = createStackNavigator(
+  {
+    HelloWorld: {screen: HelloWorld}
   },
-});
+  {
+    initialRouteName: 'HelloWorld',
+    headerMode: 'none'
+  }
+);
+
+export default App;
